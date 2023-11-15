ed24c5e788b4c06660d478eb9c0f94ed8da30afc
assemanur <asemyara.n@gmail.com>
2023-11-15 14:43:12 -0800

    initiated server.py models.py and html templates

---
+++ models.py
@@ -0,0 +1,79 @@
+from flask_sqlalchemy import SQLAlchemy
+from server import app
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    lists = db.relationship('ToDoList', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class ToDoList(db.Model):
+
+    __tablename__ = 'todo_lists'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    items = db.relationship('ToDoItem', backref='todolist', lazy=True)
+
+    def __repr__(self):
+        return f'<ToDoList {self.title}>'
+    
+class ToDoItem(db.Model):
+
+    __tablename__ = 'todo_items'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    is_completed = db.Column(db.Boolean, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('to_do_list.id'), nullable=False)
+    comments = db.relationship('Comment', backref='todoitem', lazy=True)
+
+    def __repr__(self):
+        return f'<ToDoItem {self.description}>'
+    
+class Category(db.Model):
+
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    lists = db.relationship('ToDoList', backref='category', lazy=True)
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+    
+class Comment(db.Model):
+
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('to_do_item.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Comment {self.content}>'
+    
+
+def connect_to_db(flask_app, db_uri="postgresql:///todo_list", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+    
+if __name__ == "__main__":
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
---
+++ server.py
@@ -0,0 +1,32 @@
+"""Server for a To-Do List App website."""
+
+from flask import (Flask, render_template, request, flash, session, redirect)
+from flask_sqlalchemy import SQLAlchemy
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+app.secret_key = 'dev'
+app.jinja_env.undefined = StrictUndefined
+
+# Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    """View the homepage."""
+    return render_template('home.html')
+
+@app.route('/signin')
+def signin():
+    """Sign-in to the account."""
+    return render_template('signin.html')  # You need to create a signin.html template
+
+@app.route('/signup')
+def signup():
+    """Sign up for an account."""
+    return render_template('signup.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
