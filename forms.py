c0bba1c4b33ba3c139f90152adbeb8d0086e2961
assemanur <asemyara.n@gmail.com>
2023-11-15 19:08:13 -0800

    added password reset, to-do list, and to-do item wtforms

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 """WTForms form classes."""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, Optional
 
 
@@ -24,4 +24,26 @@ class UpdateUserForm(FlaskForm):
     email = StringField('Email', validators=[Optional(), Email()])
     password = PasswordField('Password', validators=[Optional(), Length(min=8, max=35)])
 
+
+
+class PasswordResetForm(FlaskForm):
+    new_password = PasswordField('New Password', validators=[DataRequired(), Length(min=8, max=35)])
+    confirm_password = PasswordField('Confirm New Password', validators=[DataRequired(), EqualTo('new_password')])
+    submit = SubmitField('Reset Password')
+
+
+class ToDoListForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=100)])
+    description = TextAreaField('Description', validators=[Length(max=500)])
+    user_id = IntegerField('User ID', validators=[DataRequired()])
+    category = StringField('Category', validators=[Optional()])
+    submit = SubmitField('Create List')
+
+
+class ToDoItemForm(FlaskForm):
+    description = StringField('Description', validators=[DataRequired(), Length(max=200)])
+    comment = TextAreaField('Comment', validators=[Optional()])
+    is_completed = BooleanField('Completed', validators=[Optional()])
+    list_id = IntegerField('List ID', validators=[DataRequired()])
+    submit = SubmitField('Add Item')
 
