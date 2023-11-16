b6f5ada597f55f23f7cc49b1c85bbeb8a1789328
assemanur <asemyara.n@gmail.com>
2023-11-15 16:02:05 -0800

    minor edit

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-"""Models for the database."""
+"""SQLAlchemy models."""
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from server import app
